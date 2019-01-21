f1e7f03e987f847b06eb8b223262c155dcfe37c8
Faïcel Chamroukhi <faicel.chamroukhi@gmail.com>
2019-01-21 16:53:57 +0100

    plot original data and optimise figure display

--- show_MixRHLP_results.m
+++ show_MixRHLP_results.m
@@ -11,7 +11,14 @@ colors = {'r','b','g','m','c','k','y'};
 % colors_cluster_means = {'m','c','k','b','r','g'};
 colors_cluster_means = {[0.8 0 0],[0 0 0.8],[0 0.8 0],'m','c','k','y'};
 
-figure
+scrsz = get(0,'ScreenSize');
+figure('Position',[1 scrsz(4)/2 560 scrsz(4)/2.15]);
+plot(t, data');
+title('Original time series')
+ylabel('y')
+xlabel('Time')
+
+figure('Position',[scrsz(4)/2 scrsz(4)/2 560 scrsz(4)/2.15]);
 %%%% cluster and means
 for g=1:G
     cluster_g = data(solution.klas==g,:);
@@ -20,6 +27,7 @@ for g=1:G
 %     plot(t,solution.Ex_g(:,g),'color',colors{g},'linewidth',3)
     plot(t,solution.Ex_g(:,g),'color',colors_cluster_means{g},'linewidth',3)
 end
+title('Clustered and segmented time series')
 ylabel('y')
 xlabel('Time')
 
@@ -51,8 +59,7 @@ for g=1:G
     cluster_g = data(solution.klas==g,:); 
     
     %%%     figure(g+1)
-    scrsz = get(0,'ScreenSize');
-    figr = figure('Position',[1 scrsz(4)/2 560 scrsz(4)/1.4]);
+    figr = figure('Position',[scrsz(4) scrsz(4)/2 560 scrsz(4)/1.4]);
     axes1 = axes('Parent',figr,'Position',[0.13 0.45 0.775 0.48],'FontSize',14);
     box(axes1,'on'); hold(axes1,'all');
     %
@@ -78,71 +85,4 @@ for g=1:G
     
      xlim([0 m-1])
 end
-
-
-
-%
-% figure(2),
-% G = length(solution.param.alpha_g);
-% for g=1:G
-%     subplot(G,1,g),plot(solution.tau_ijgk(:,:,g))
-%     title(['\tau_{ ijgk} , g = ',int2str(g)])
-% end
-% figure(3),
-% plot(solution.h_ig);
-%
  
-% for g=1:G
-%     figure,plot(data(solution.klas==g,:)','color',colors{g},'LineWidth',1)
-%     hold on,plot(solution.Ex_g(:,g),'black','LineWidth',3) 
-% end
-
-
-%  
-  
-% for g=1:G
-%     %subplot(G+1,1,1),
-% %     plot(t,data(solution.klas==g,:)','color',colors{g})%,t,solution.Ex_g(:,g),'black')
-% %     hold on
-% %     su% figure
-% for g=1:G
-%     %plot(t,data(solution.klas==g,:)','color',colors{g},'LineWidth',1)    
-%     plot(t,data(solution.klas==g,:)','color',[0.5 0.5 0.5]+.1*(g-1))
-%         hold on
-%     %plot(t,X','color',[0.5 0.5 0.5]);
-% end
-% for g=1:G
-%     hold on,plot(t,solution.Ex_g(:,g),'color','r','LineWidth',3) %'black','LineWidth',3) 
-%     hold on
-% end
-% plot(G+1,1,g+1),
-%     figure,plot(t,solution.param.pi_jgk(1:m,:,g),'linewidth',2);
-%     ylabel(['\pi_{jk}( w^g) , g = ',int2str(g)])
-%     xlabel('j')
-%     set(gca,'ytick',[0:0.2:1])
-% end
-%  
-% %
-% % figure(2),
-% % G = length(solution.param.alpha_g);
-% % for g=1:G
-% %     plot(solution.tau_ijgk(:,:,g))
-% %     title(['\tau_{ ijgk} , g = ',int2str(g)])
-% % end
-% 
-
-% figure(G+1),
-% figure
-% for g=1:G
-%     %plot(t,data(solution.klas==g,:)','color',colors{g},'LineWidth',1)    
-%     plot(t,data(solution.klas==g,:)','color',[0.5 0.5 0.5]+.1*(g-1))
-%         hold on
-%     %plot(t,X','color',[0.5 0.5 0.5]);
-% end
-% for g=1:G
-%     hold on,plot(t,solution.Ex_g(:,g),'color','r','LineWidth',3) %'black','LineWidth',3) 
-%     hold on
-% end
-
-%  xlabel('Time')
-% ylabel('y_{ij}')
